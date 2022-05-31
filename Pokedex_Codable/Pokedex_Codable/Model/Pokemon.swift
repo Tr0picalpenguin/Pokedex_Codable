dfd91088e7663de16c79edcf415e8af6a2c72ba0
Scott Cox <jscox5@gmail.com>
2022-05-31 10:45:20 -0600

    initial commit with model complete

--- Pokedex_Codable/Pokedex_Codable/Model/Pokemon.swift
+++ Pokedex_Codable/Pokedex_Codable/Model/Pokemon.swift
@@ -7,7 +7,17 @@
 
 import Foundation
 
-class Pokemon {
+// Top Level Dictionary
+struct Pokedex: Decodable {
+    let results: [ResultsDictionary]
+}
+
+struct ResultsDictionary: Decodable {
+    let name: String
+    let url: String
+}
+
+struct Pokemon: Decodable {
     enum Keys: String {
         case name = "name"
         case id = "id"
@@ -19,29 +29,20 @@ class Pokemon {
     
     let name: String
     let id: Int
-    let moves: [String]
-    let spritePath: String
-    
-    init?(dictionary:[String:Any]) {
-        guard let name = dictionary[Keys.name.rawValue] as? String,
-              let id = dictionary[Keys.id.rawValue] as? Int,
-              let spriteDict = dictionary[Keys.sprites.rawValue] as? [String:Any],
-              let spritePosterPath = spriteDict[Keys.frontShiny.rawValue] as? String,
-              let movesArray = dictionary[Keys.moves.rawValue] as? [[String:Any]] else {return nil}
-        
-        var moves: [String] = []
-        
-        for dict in movesArray {
-            guard let moveDict = dict[Keys.move.rawValue] as? [String:Any],
-                  let moveName = moveDict[Keys.name.rawValue] as? String else {return nil}
-            
-            moves.append(moveName)
-        }
-        
-        self.name = name
-        self.id = id
-        self.moves = moves
-        self.spritePath = spritePosterPath
+    let sprites: Sprites
+}
+
+struct Sprites: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case frontDefault = "front_default"
+        case frontShiny = "front_shiny"
+        case frontFemale = "front_female"
+        case frontShinyFemale = "front_shiny_female"
         
     }
+    
+    let frontDefault: String
+    let frontShiny: String
+    let frontFemale: String?
+    let frontShinyFemale: String?
 }
